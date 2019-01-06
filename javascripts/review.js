efab2cc758ce7a215446b0230826f9e1aae60d5b
javier <javcoz@yahoo.com>
2019-01-06 02:17:30 -0500

    commit from phone

--- javascripts/review.js
+++ javascripts/review.js
@@ -73,7 +73,7 @@ function MatchWordsOrPhrases(){
 	
 	
 	// Clear page and populate it with new stuff to give the feeling of a new window
-	document.getElementById("question").innerHTML = "";
+	document.getElementById("question").innerHTML = "<br>";
     document.getElementById("instruction").innerHTML = "<h4>Select a Cathegory</h4>";
     document.getElementById("question").style.display = "block";
     document.getElementById("answer").innerHTML = "";

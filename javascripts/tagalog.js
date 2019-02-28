3619a6fc1d712fa742625f012d590fd9a902b6df
javier <javcoz@yahoo.com>
2019-02-27 23:49:08 -0500

    fixed typo again. darn!

--- javascripts/tagalog.js
+++ javascripts/tagalog.js
@@ -11,7 +11,7 @@ function refreshPage(){ // Window Resizing makes new page dimentions
     }
 
 function init(){
-	document.getElementById("vocab_link").click();
+	document.getElementById("review_link").click();
     }
 
 function openTab(evt, tabName) {
